8cb87295f2ac0745e9c1f219b7dc0438b06799d8
Reed O'Brien <reed@koansys.com>
2012-05-25 18:05:42 -0400

    add bits to setup splash page
    [#29802925]

--- kotti_splashpage/__init__.py
+++ kotti_splashpage/__init__.py
@@ -1,1 +1,11 @@
-# a package
+from kotti.resources import get_root
+
+def populate():
+    site = get_root()
+    site.default_view = 'splash-page'
+
+def includeme(config):
+    config.add_view(
+        name='splash-page',
+        renderer='kotti_splashpage:templates/splash-page.pt',
+    )
