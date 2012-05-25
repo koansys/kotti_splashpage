1bd21c5003ecb5ac570f4a8dbf1b2d17fe228435
Reed O'Brien <reed@koansys.com>
2012-05-25 16:41:47 -0400

    Initial empty package
    [#29802925]

---
+++ README.rst
@@ -0,0 +1,17 @@
+kotti_splashpage README
+=======================
+
+Description
+-----------
+
+`kotti_splashpage` is a kotti package to setup the home page as a splash page.
+
+Installation
+------------
+
+TODO: ...
+
+Development
+-----------
+
+TODO: ...
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'kms',
+    ]
+
+setup(name='kotti_splashpage',
+      version='0.1.0',
+      description='kotti_splashpage',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="kotti_splashpage",
+      entry_points="""\
+      [paste.app_factory]
+      main = kotti_splashpage:main
+      """,
+      )
