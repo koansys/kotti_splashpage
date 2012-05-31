d9d47dbd462c674d9911c1c0b8cbf8d3f1bc440d
Reed O'Brien <reed@koansys.com>
2012-05-31 11:40:29 -0400

    move populate into include me
    [#29802925]
    
    Should there be a test for the populators append bit in includeme? likely

--- kotti_splashpage/__init__.py
+++ kotti_splashpage/__init__.py
@@ -7,6 +7,16 @@ def populate():
 
 
 def includeme(config):
+    settings = config.get_settings()
+    populators = settings.get('kotti.populators', [])
+    if populators:
+        if not populate in populators:
+            ## if it is not in settings add it we let it be. If it is in populators
+            ## because we *know* the user set the populators order on purpose.
+            ## Otherwise we add it.
+            settings['kotti.populators'].append(populate)
+    else:
+        settings['kotti.populators'] = [populate, ]
     config.add_view(
         name='splash-page',
         renderer='kotti_splashpage:templates/splash-page.pt',
